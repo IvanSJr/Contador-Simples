41c5c31fd43da0585cd0fa76ac2043d9a8820ddd
Ivan Júnior <ivan.junior2706@gmail.com>
2021-06-03 16:51:41 -0300

    Iniciando meu projeto Vue JS

---
+++ projeto_cli/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+      <h1>Bem vindo ao meu primeiro projeto</h1>
+
+      <Usuario/>
+
+  </div>
+</template>
+
+<script>
+import Usuario from './components/Usuario';
+export default {
+  name: 'App',
+  components:{
+    Usuario
+  },
+  data(){
+    return{
+      nome: "Ivan Santos de Jesus Júnior"
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ projeto_cli/src/components/Usuario.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="usuario">
+        <h2>O usuário é: Ivan</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Usuario'
+}
+</script>
+
+<style>
+    h2{
+        color:blue;
+    }
+</style>
